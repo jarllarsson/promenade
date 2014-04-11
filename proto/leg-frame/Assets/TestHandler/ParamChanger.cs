24c6208bfaf94d6be0f5f215de2bf572263a8995
jarllarsson <jarl.larsson@gmail.com>
2014-04-11 12:02:56 +0200

    Working on optimization and iterations handling

---
+++ proto/leg-frame/Assets/TestHandler/ParamChanger.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ParamChanger
+{
+    private UniformDistribution m_uniformDistribution;
+
+    public ParamChanger()
+    {
+        m_uniformDistribution = new UniformDistribution();
+        UnityEngine.Random.seed = (int)Time.time;
+    }
+
+    public List<float> change(List<float> p_params)
+    {
+        int size = p_params.Count;
+        float[] result = new float[size];
+        List<double> deltaP = getDeltaP(p_params);
+        for (int i = 0; i < size; i++)
+        {
+            result[i] = (float)((double)p_params[i] + deltaP[i]);
+        }
+        return new List<float>(result);
+    }
+
+    /// <summary>
+    /// Selection vector, determines wether the parameter
+    /// at this position in the list should be changed.
+    /// 20% probability of change.
+    /// </summary>
+    /// <param name="p_size"></param>
+    /// <returns></returns>
+	private List<float> getS(int p_size)
+    {
+        int changeProbabilityPercent = 20;
+        float[] S = new float[p_size];
+        for (int i=0;i<p_size;i++)
+        {
+            S[i] = UnityEngine.Random.Range(0, 99) < changeProbabilityPercent ? 1.0f : 0.0f;
+        }
+        return new List<float>(S);
+    }
+
+    /// <summary>
+    /// Generate DeltaP the change vector to be added to old P.
+    /// It contains randomly activated slots. 
+    /// Not all parameters will thus be changed by this vector.
+    /// </summary>
+    /// <param name="p_P"></param>
+    /// <returns></returns>
+    private List<double> getDeltaP(List<float> p_P)
+    {
+        int size=p_P.Count;
+        // Get R value
+        double Pmax = 0.0f, Pmin = 0.0f;
+        //getMaxMinOfList(p_P, out Pmin, out Pmax);
+        double r = 40.0f;
+        r = Random.Range(0.0f, 40.0f);
+        Pmax = r; Pmin = -r;
+        double R = Pmax - Pmin;
+        //Debug.Log("R: " + R + " Pmax: " + Pmax + " Pmin: " + Pmin);
+
+        // Get S vector
+        List<float> S = getS(size);
+
+        // Calculate delta-P
+        double[] deltaP = new double[size];
+        for (int i = 0; i < size; i++)
+        {
+            double P=(double)p_P[i];
+            double c=m_uniformDistribution.U(P - 0.1 * R, P + 0.1 * R);
+            deltaP[i] = (double)S[i] * c;
+        }
+        return new List<double>(deltaP);
+    }
+
+    void getMaxMinOfList(List<float> p_list, out float p_min, out float p_max)
+    {
+        p_min = 999999999.0f;
+        p_max = 0.0f;
+        for (int i=0;i<p_list.Count;i++)
+        {
+            if (p_list[i] > p_max) p_max = p_list[i];
+            if (p_list[i] < p_min) p_min = p_list[i];
+        }
+    }
+
+   
+}
---
+++ proto/leg-frame/Assets/TestHandler/TestHandler.cs
@@ -0,0 +1,172 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TestHandler : MonoBehaviour 
+{
+    public Controller[] m_optimizableControllers;
+    private ParamChanger m_changer;
+    public float m_simTime = 1.0f;
+    private float m_currentSimTime = 0.0f;
+    public bool m_instantEval = false;
+
+    private int m_currentBestCandidate = -1;
+    private int m_drawBestCandidate = -1;
+
+    private double m_lastBestScore = double.MaxValue;
+    List<float> m_lastBestParams;
+    List<List<float>> m_currentParams;
+
+    private double[] m_totalScores;
+    // Use this for initialization
+    void Start()
+    {
+        m_changer = new ParamChanger();
+        m_currentParams = new List<List<float>>();
+        m_currentBestShowcase = new List<float>();
+        m_totalScores = new double[m_optimizableControllers.Length];
+        StoreParams();
+        ResetScores();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < 200; i++)
+        {
+            EvaluateAll();
+            m_currentSimTime += Time.deltaTime;
+            if (m_instantEval || m_currentSimTime >= m_simTime)
+            {
+                FindCurrentBestCandidate();
+                if (m_currentBestCandidate >= 0)
+                    Debug.Log("New best candidate was: " + m_currentBestCandidate);
+                if (m_lastBestScore > 0.01f)
+                    PerturbParams();
+                RestartSim();
+                // Possible scene restart here <-
+            }
+        }
+    }
+
+    private void RestartSim()
+    {
+        m_currentSimTime = 0.0f;
+        StoreParams();
+        VoidBestCandidate();
+        ResetScores();
+    }
+
+    private void FindCurrentBestCandidate()
+    {
+        VoidBestCandidate();
+        double bestScore = m_lastBestScore;
+        //Debug.Log("R"+bestScore);
+        for (int i = 0; i < m_totalScores.Length; i++)
+        {
+            //Debug.Log("NR " + m_totalScores[i]);
+            if (m_totalScores[i] < bestScore)
+            {
+                m_currentBestCandidate = i;
+                bestScore = m_totalScores[i];
+            }
+        }
+        m_lastBestScore = bestScore;
+        if (m_currentBestCandidate > -1)
+            m_drawBestCandidate = m_currentBestCandidate;
+    }
+
+    private void VoidBestCandidate()
+    {
+        m_currentBestCandidate = -1;
+    }
+
+    private void StoreParams()
+    {
+        m_currentParams.Clear();
+        for (int i = 0; i < m_optimizableControllers.Length; i++)
+        {
+            m_currentParams.Add(m_optimizableControllers[i].GetParams());
+        }
+    }
+
+    private void ResetScores()
+    {
+        for (int i = 0; i < m_totalScores.Length; i++)
+        {
+            m_totalScores[i] = 0.0f;
+        }
+    }
+
+    private void PerturbParams()
+    {
+        // Get params from winner and use as basis for perturbing
+        // Only update params if they were better than before, else reuse old
+        if (m_currentBestCandidate > -1)
+        {
+            //IOptimizable best = m_optimizableControllers[m_currentBestCandidate];
+            m_lastBestParams = m_currentParams[m_currentBestCandidate];
+        }
+        // Perturb and assign to candidates
+        for (int i = 0; i < m_optimizableControllers.Length; i++)
+        {
+            IOptimizable opt = m_optimizableControllers[i];
+            List<float> newObjParams = m_changer.change(m_lastBestParams); // different perturbation to each
+            // Debug.Log("new params for "+i+" sample at i=10:  _" + newObjParams[10]+"_");
+            opt.ConsumeParams(newObjParams); // consume it to controller
+        }
+    }
+
+    private void EvaluateAll()
+    {
+        for (int i = 0; i < m_optimizableControllers.Length; i++)
+        {
+            m_totalScores[i] += GetCandidateFitness(i);
+        }
+    }
+
+    private double GetCandidateFitness(int p_idx)
+    {
+        return m_optimizableControllers[p_idx].EvaluateFitness();
+    }
+
+    public void drawParamGraphs(int p_idx)
+    {
+        Vector2 s = new Vector2(transform.localScale.x,transform.localScale.y);
+        Vector3 pos = m_optimizableControllers[p_idx].transform.position+Vector3.up*6;
+        for (int i=0;i<m_currentParams.Count;i++)
+            drawLineGraph(m_currentParams[i], s, m_optimizableControllers[i].transform.position);
+        drawLineGraph(m_lastBestParams, s, transform.position);
+    }
+
+    public void drawLineGraph(List<float> p_graph, Vector2 p_scale, Vector3 p_wpos)
+    {
+        for (int n = 0; n < p_graph.Count-1; n++)
+        {
+            float t = (float)n / (float)p_graph.Count;
+            float t1 = (float)(n+1) / (float)p_graph.Count;
+            float val = p_graph[n];
+            float val1 = p_graph[n+1];
+            Gizmos.color=Color.Lerp(Color.cyan,Color.magenta,val*0.1f);
+            Gizmos.DrawLine(p_wpos + Vector3.right * p_scale.x * t + Vector3.up * val * p_scale.y,
+                transform.position + Vector3.right * p_scale.x * t1 + Vector3.up * val1 * p_scale.y);
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        if (m_drawBestCandidate > -1)
+        {
+            if (m_currentBestCandidate > -1)
+            {
+                Gizmos.color = Color.green;
+                Gizmos.DrawSphere(m_optimizableControllers[m_drawBestCandidate].transform.position - Vector3.up, 0.3f);
+            }
+            else
+            {
+                Gizmos.color = Color.blue;
+                Gizmos.DrawSphere(m_optimizableControllers[m_drawBestCandidate].transform.position - Vector3.up, 0.3f);
+            }
+        }
+    }
+}
