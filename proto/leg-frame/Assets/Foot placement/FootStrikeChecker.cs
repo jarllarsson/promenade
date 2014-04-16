9d1bb43bc4e61a4f304f97d109e4cd9c70c48aff
jarllarsson <jarl.larsson@gmail.com>
2014-04-16 14:46:58 +0200

    Adding script to check for early or late foot strike

---
+++ proto/leg-frame/Assets/Foot placement/FootStrikeChecker.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FootStrikeChecker : MonoBehaviour 
+{
+    private bool isOnGround=true;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if (isFootStrike())
+            renderer.material.color = Color.blue;
+        else
+            renderer.material.color = Color.white;
+	}
+
+    public bool isFootStrike()
+    {
+        return isOnGround;
+    }
+
+    
+    void OnCollisionEnter()
+    {
+        isOnGround = true;
+    }
+
+    void OnCollisionExit()
+    {
+        isOnGround = false;
+    }
+}
