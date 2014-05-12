b16017026497e00e5c970491c7349b41414f62c8
jarllarsson <jarl.larsson@gmail.com>
2014-05-12 20:21:36 +0200

    simple falling test for bullet, from http://bulletphysics.org/mediawiki-1.5.8/index.php/Hello_World is working, with exact same result (as that in the wiki)

--- multileg/src/BulletPhysicsTest/main.cpp
+++ multileg/src/BulletPhysicsTest/main.cpp
@@ -26,6 +26,35 @@ int main()
 	// ==================================
 	btDiscreteDynamicsWorld* dynamicsWorld = new btDiscreteDynamicsWorld(dispatcher, broadphase, solver, collisionConfiguration);
 	dynamicsWorld->setGravity(btVector3(0, -10, 0));
+	// -----------
+	// Objects
+	// -----------
+	// Create shapes
+	btCollisionShape* groundShape = new btStaticPlaneShape(btVector3(0, 1, 0), 1);
+	btCollisionShape* fallShape = new btSphereShape(1);
+	// Create motion state for ground
+	// http://bulletphysics.org/mediawiki-1.5.8/index.php/MotionStates#MotionStates
+	// Are used to retreive the calculated transform data from bullet
+	btDefaultMotionState* groundMotionState = new btDefaultMotionState(btTransform(btQuaternion(0, 0, 0, 1), 
+																				   btVector3(0, -1, 0)));
+	// Create rigidbody for ground
+	// Bullet considers passing a mass of zero equivalent to making a body with infinite mass - it is immovable.
+	btRigidBody::btRigidBodyConstructionInfo groundRigidBodyCI(0, groundMotionState, groundShape, btVector3(0, 0, 0));
+	btRigidBody* groundRigidBody = new btRigidBody(groundRigidBodyCI);
+	// Add ground to world
+	dynamicsWorld->addRigidBody(groundRigidBody);
+
+	// Same procedure for sphere
+	// Create rigidbody for sphere (with motion state 50m above ground)
+	btDefaultMotionState* fallMotionState =
+		new btDefaultMotionState(btTransform(btQuaternion(0, 0, 0, 1), btVector3(0, 50, 0)));
+	btScalar mass = 1; // 1kg
+	btVector3 fallInertia(0, 0, 0);
+	fallShape->calculateLocalInertia(mass, fallInertia); // sphere inertia
+	// And the rigidbody
+	btRigidBody::btRigidBodyConstructionInfo fallRigidBodyCI(mass, fallMotionState, fallShape, fallInertia);
+	btRigidBody* fallRigidBody = new btRigidBody(fallRigidBodyCI);
+	dynamicsWorld->addRigidBody(fallRigidBody);
 
 
 	// ==================================
@@ -34,18 +63,40 @@ int main()
 	// ==================================
 	// ==================================
 
+	// Simulate for 300 iterations
+	for (int i = 0; i<300; i++) 
+	{
+		dynamicsWorld->stepSimulation(1 / 60.f, 10); // 60hz
 
+		btTransform trans;
+		fallRigidBody->getMotionState()->getWorldTransform(trans);
+
+		std::cout << "sphere height: " << trans.getOrigin().getY() << std::endl;
+	}
 
 
 	// ==================================
 	// ==================================
 
 	// Bullet has a policy of "whoever allocates, also deletes" memory
+	dynamicsWorld->removeRigidBody(fallRigidBody);
+	delete fallRigidBody->getMotionState();
+	delete fallRigidBody;
+
+	dynamicsWorld->removeRigidBody(groundRigidBody);
+	delete groundRigidBody->getMotionState();
+	delete groundRigidBody;
+
+	delete groundShape;
+	delete fallShape;
+
 	delete dynamicsWorld;
 	delete solver;
 	delete dispatcher;
 	delete collisionConfiguration;
 	delete broadphase;
+
+
 
 	return 0;
 }
