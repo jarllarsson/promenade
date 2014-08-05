a0e92e2fea98b056c78db78b509f4b6ef832101d
Jarl Larsson <jarl.larsson@gmail.com>
2014-08-05 17:36:46 +0200

    rewritten ReferenceLegMovementController

--- multileg/src/winapp/ReferenceLegMovementController.h
+++ multileg/src/winapp/ReferenceLegMovementController.h
@@ -67,53 +67,46 @@ public:
 // 			m_liftPos[i] = m_foot[i].position;
 // 		}
 
-	// Update is called once per frame
-	void iterate(float p_dt)
+
+
+	void updateRefPositions(const glm::vec3& p_lfPos, float p_lfHeight, float p_uLegLen, float p_lLegLen, float p_dt, DebugDrawBatch* p_drawer)
 	{
 		// Advance the player
 		m_player.updatePhase(p_dt);
-
-		updateFeetPositions(m_player.getPhase());
-		updateKneeRefs();
-	}
-
-	void updateKneeRefs()
-	{
-		for (int i = 0; i < m_knees.size(); i++)
+		float phi = m_player.getPhase();
+		//
+		for (int i = 0; i < m_feet.size(); i++)
 		{
-			m_knees[i] = m_IK[i].m_kneePosW;
-		}
-	}
-
-
-	void updateFeetPositions(float p_phi)
-	{
-		for (int i = 0; i < m_foot.Length; i++)
-		{
-			bool inStance = m_stepCycles[i].isInStance(p_phi);
-			//
+			bool inStance = m_stepCycles[i].isInStance(phi);
+			//				
+			float flip = (i * 2.0f) - 1.0f;
+			glm::vec3 baseOffset(flip*m_stepLength.x, 0.0f, 0.0f);
+			glm::vec3 lfPlaneBase(p_lfPos.x, 0.0f, p_lfPos.z);
 			if (!inStance)
 			{
-				float swingPhi = m_stepCycles[i].getSwingPhase(p_phi);
+				float swingPhi = m_stepCycles[i].getSwingPhase(phi);
 				// The height offset, ie. the "lift" that the foot makes between stepping points.
-				Vector3 heightOffset = new Vector3(0.0f, m_stepHeightTraj.getValAt(swingPhi), 0.0f);
-				float flip = (i * 2.0f) - 1.0f;
-				Vector3 wpos = Vector3.Lerp(m_liftPos[i],
-					transform.position + new Vector3(flip*m_stepLength.x, 0.0f, m_stepLength.y*0.5f),
+				glm::vec3 heightOffset(0.0f, m_stepHeightTraj.lerpGet(swingPhi), 0.0f);
+
+				glm::vec3 wpos = glm::lerp(m_liftPos[i],
+					p_lfPos + glm::vec3(baseOffset.x, 0.0f, m_stepLength.y*0.5f),
 					swingPhi);
-				wpos = new Vector3(wpos.x, 0.0f, wpos.z);
-				m_foot[i].position = wpos + heightOffset;
+				wpos = glm::vec3(wpos.x, 0.0f, wpos.z);
+				m_feet[i]= wpos + heightOffset;
 
 			}
 			else
 			{
-				m_liftPos[i] = m_foot[i].position;
-				Debug.DrawLine(m_foot[i].position, m_foot[i].position + Vector3.up, Color.magenta - new Color(0.3f, 0.3f, 0.3f, 0.0f), 10.0f);
+				m_liftPos[i] = m_feet[i];
+				//Debug.DrawLine(m_foot[i].position, m_foot[i].position + Vector3.up, Color.magenta - new Color(0.3f, 0.3f, 0.3f, 0.0f), 10.0f);
 			}
-			Color debugColor = Color.red;
-			if (i == 1) debugColor = Color.green;
-			Debug.DrawLine(m_oldFootPos[i], m_foot[i].position, debugColor, 30.0f);
-			m_oldFootPos[i] = m_foot[i].position;
+			//Color debugColor = Color.red;
+			//if (i == 1) debugColor = Color.green;
+			//Debug.DrawLine(m_oldFootPos[i], m_foot[i].position, debugColor, 30.0f);
+			m_oldFeetPos[i] = m_feet[i];
+			m_IK.solve(m_feet[i], baseOffset + lfPlaneBase, p_uLegLen, p_lLegLen, p_drawer);
+			//
+			m_knees[i] = m_IK.getKneePosL() + lfPlaneBase;
 		}
 	}
 
